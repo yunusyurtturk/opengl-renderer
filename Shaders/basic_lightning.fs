8ed432343e7cc482089f640a180fb22c63618594
yunusyurtturk <yunusyurtturk@gmail.com>
2024-04-07 21:09:04 +0300

    Missing shaders added

---
+++ Shaders/basic_lightning.fs
@@ -0,0 +1,25 @@
+#version 330
+
+in vec3 Normal;  
+in vec3 FragPos;  
+
+uniform vec3 lightPos; 
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+out vec4 FragColor;
+
+
+void main()
+{
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    vec3 result = (ambient + diffuse) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
---
+++ Shaders/basic_lightning.vs
@@ -0,0 +1,22 @@
+#version 330
+
+layout (location = 0) in vec3 aPosition;
+layout (location = 1) in vec3 aNormal;
+
+uniform mat4 gVP;
+uniform mat4 gWP;
+
+uniform vec3 lightPos; 
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+out vec3 FragPos;
+out vec3 Normal;
+
+void main()
+{
+    FragPos = vec3(gWP * vec4(aPosition, 1.0));
+    Normal = aNormal;  
+
+    gl_Position =  gVP * vec4(FragPos, 1.0);
+}
