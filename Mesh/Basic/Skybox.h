907b67a95fab1bd3fb31f36cef80bb9907faa5ed
yunusyurtturk <yunusyurtturk@gmail.com>
2024-05-05 23:55:50 +0300

    Cubemap/skybox shaders added

---
+++ Mesh/Basic/Skybox.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "../Mesh.h"
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include "../../CompiledShaderProgram.h"
+
+class Skybox : public Mesh
+{
+private:
+	static const int VERTEX_COUNT = 36;
+    static const int VERTEX_ATTR_COUNT = 3;
+	const float vertices[VERTEX_COUNT * VERTEX_ATTR_COUNT] = {
+        // positions          
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f
+    };
+
+	void initIndex()
+	{
+
+	}
+public:
+	Skybox() : Mesh()
+	{
+		std::vector<Vertex>& Vertices = GetVertex();
+		Vertices.resize(VERTEX_COUNT);
+
+		for (int i = 0; i < VERTEX_COUNT; i++) {
+			int line = i * VERTEX_ATTR_COUNT;
+			Vertices[i] = Vertex(
+				vertices[line + 0], vertices[line + 1], vertices[line + 2],
+				0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f);
+		}
+	}
+
+	virtual void Draw(CompiledShaderProgram& shader) override
+	{
+		glBindVertexArray(VAO);
+        glActiveTexture(GL_TEXTURE0);
+		glDrawArrays(GL_TRIANGLES, 0, VERTEX_COUNT);
+		glBindVertexArray(0);
+
+		
+	}
+};
---
+++ Shaders/Skybox/shader_skybox.vs
@@ -0,0 +1,15 @@
+#version 330
+
+layout (location = 0) in vec3 aPosition;
+
+uniform mat4 gVP;
+uniform mat4 gWP;
+
+out vec3 TexCoord;
+
+void main()
+{
+    TexCoord = aPosition;
+    vec4 pos = gVP * vec4(aPosition, 1.0);
+    gl_Position = pos.xyww;
+}
